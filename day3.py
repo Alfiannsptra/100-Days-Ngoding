14f3e2c69717e226d35d34983027d725f0c0cd30
Alfiannsptra <alfiansaputra355@gmail.com>
2023-10-11 20:17:14 +0800

    day3 python

---
+++ day3.py
@@ -0,0 +1,8 @@
+# Program Python dengan tipe data "char" (menggunakan string)
+char_variable = 'A'
+
+# Menampilkan nilai variabel
+print("Nilai variabel char:", char_variable)
+
+# Menampilkan tipe data variabel
+print("Tipe data variabel:", type(char_variable))
