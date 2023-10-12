7bef493a655f4e9829d308960630af27448b1655
Alfiannsptra <alfiansaputra355@gmail.com>
2023-10-12 17:19:20 +0800

    day4 python

---
+++ day4.py
@@ -0,0 +1,9 @@
+# Program Python dengan tipe data "double" (menggunakan float)
+# menggunakan tipe data float untuk merepresentasikan bilangan pecahan, dan Python tidak memiliki tipe data double secara eksplisit karena bilangan pecahan di Python sudah diimplementasikan menggunakan representasi double-precision.
+double_variable = 3.14
+
+# Menampilkan nilai variabel
+print("Nilai variabel double:", double_variable)
+
+# Menampilkan tipe data variabel
+print("Tipe data variabel:", type(double_variable))
