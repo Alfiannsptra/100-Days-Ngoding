9eea7ba2464306ea1d8c4e82771d74dbd79ba552
Alfiannsptra <alfiansaputra355@gmail.com>
2023-10-13 21:29:40 +0800

    day5 python

---
+++ day5.py
@@ -0,0 +1,8 @@
+# Program Python dengan tipe data "integer"
+angka_bulat = 42
+
+# Menampilkan nilai variabel
+print("Nilai variabel integer:", angka_bulat)
+
+# Menampilkan tipe data variabel
+print("Tipe data variabel:", type(angka_bulat))
