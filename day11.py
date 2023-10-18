fb9a2570650f2027d5b4c6db962a7391242b5f19
Alfiannsptra <alfiansaputra355@gmail.com>
2023-10-19 02:35:12 +0800

    day 11 program menghitung berapa lama sambo membeli PC GAMING

---
+++ day11.py
@@ -0,0 +1,12 @@
+pcGaming = 150000000
+maksimalDitabungPerBulan = 5000000
+setahun = 12
+
+jumlah = maksimalDitabungPerBulan * setahun
+totalTahun = pcGaming / jumlah
+print("============================================")
+print("SAMBO INGIN MENABUNG UNTUK MEMBELI PC GAMING")
+print("============================================")
+print("+ Harga PC GAMING IDAMAN SAMBO :Rp.150.000.000")
+print("+ Maksimal yang SAMBO dapat tabung dari gaji bulanannya yang sebesar Rp.13.000.000 adalah :Rp.5.000.000/bulan")
+print("+ Setelah dihitung, SAMBO akan membeli PC GAMING idamannya dengan menabung selama kurang lebih :", totalTahun, "Tahun")
