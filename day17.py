c1fd48eefde6d0a1acd6a6d61b0f98d359c18afa
Alfiannsptra <alfiansaputra355@gmail.com>
2023-10-25 22:34:11 +0800

    day 17 Program menghitung luas persegi panjang menggunakan function def

---
+++ day17.py
@@ -0,0 +1,5 @@
+#membuat menghitung luas dengan function def
+def hitung_luas(panjang,lebar):
+    return panjang * lebar
+#memanggil function serta mendeklarasikan
+print("Luas :",hitung_luas(12,10))
