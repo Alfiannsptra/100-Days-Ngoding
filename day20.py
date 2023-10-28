6ec72b1f3599f64532b8877459e3717c478153b2
Alfiannsptra <alfiansaputra355@gmail.com>
2023-10-29 02:33:14 +0800

    day 20 Program gabungan def, if dan for mencetak nama

---
+++ day20.py
@@ -0,0 +1,11 @@
+def cetakNama():
+    nama = input("Masukkan nama Anda: ")
+    if nama == "alfian":
+        for i in range(10):
+            print(nama)
+    elif nama == "saputra":
+        for i in range(5):
+            print(nama)
+    else:
+        print("nama tidak ada")
+cetakNama()
