b113f687212e3b862a0a196b51ecd38971734d45
Alfiannsptra <alfiansaputra355@gmail.com>
2023-10-30 22:01:27 +0800

    day 22 Login

---
+++ day22.py
@@ -0,0 +1,6 @@
+nama = input("masukkan nama anda :")
+password = input("Masukkan password :")
+if nama == 'alfian' and password == 'pian123':
+    print("Login berhasil")
+else:
+    print("Salah kasih masuk ki ner")
