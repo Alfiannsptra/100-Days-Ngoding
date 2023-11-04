0ae55c593ce2d0a4fd341e892a7381b01e52b7f4
Alfiannsptra <alfiansaputra355@gmail.com>
2023-11-04 23:26:08 +0800

    day 27 Menampilkan isi dari array menggunakan for

---
+++ day27.py
@@ -0,0 +1,6 @@
+# Membuat array
+menu = ["es teh dondori", "kopi susu rute", "kopi susu dondori", "ayam lalapan", "nasi ayam crispi arema"]
+
+# Menggunakan loop for untuk memanggil setiap elemen dalam array
+for isi in menu:
+    print(isi)
