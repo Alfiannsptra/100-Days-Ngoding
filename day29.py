a9198ddd626feb7037aede79f7f18f16cc9213e6
Alfiannsptra <alfiansaputra355@gmail.com>
2023-11-07 10:32:58 +0800

    day 29 Menambahkan objek ke dalam list menggunakan insert pada indeks tertentu

---
+++ day29.py
@@ -0,0 +1,5 @@
+hewan = ["sapi","kucing","kuda","baruang","harimau","semut"]
+
+#menambahkan kambing ke dalam list hewan menggunakan insert pada posisi indeks tertentu
+hewan.insert(1,"kambing") 
+print(hewan)
