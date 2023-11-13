da2bbfa18333b730c61d00aafac5ab5acacd9282
Alfiannsptra <alfiansaputra355@gmail.com>
2023-11-13 16:03:18 +0800

    day 35 Latihan soal UK

---
+++ day35.py
@@ -0,0 +1,11 @@
+angka = int(input("masukkan angka: "))
+jumlah = 0
+arr =[]
+for i in range(1,angka):
+    if i % 3 == 0:
+        arr.append(i)
+        print(arr)
+        jumlah+=i
+        print(i)
+        print("-------")
+print("total penjumlahan:  ",jumlah)
