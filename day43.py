a6409adade7cd08a44f6777a07143fc4a1bc7e30
alfian <99625368+Alfiannsptra@users.noreply.github.com>
2023-11-20 18:58:11 +0800

    Add files via upload

---
+++ day43.py
@@ -0,0 +1,4 @@
+dict = {'Nama': 'Alfian', 'Umur': 21, 'Pekerjaan': 'Mahasiswa'}
+print ("dict['Nama']: ", dict['Nama'])
+print ("dict['Umur']: ", dict['Umur'])
+print ("dict['Pekerjaan']: ", dict['Pekerjaan'])
