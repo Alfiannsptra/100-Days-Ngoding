3dde268e7b59ba2e58e4d3f6a851ba67b704bcc3
alfian <99625368+Alfiannsptra@users.noreply.github.com>
2023-11-25 19:36:28 +0800

    Add files via upload

---
+++ day48.py
@@ -0,0 +1,9 @@
+bilangan = int(input("Masukkan bilangan: "))
+
+if bilangan % 2 == 0:
+    print(f"{bilangan} adalah bilangan genap.") 
+else:
+    print(f"{bilangan} adalah bilangan ganjil.")
+
+#fungsi dari print f
+#memungkinkan menyisipkan nilai variabel langsung ke dalam string menggunakan kurung kurawal {}
