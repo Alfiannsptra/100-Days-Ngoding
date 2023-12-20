0e41a2e3a2f1be896d45dc537a81e44059a6a233
alfian <99625368+Alfiannsptra@users.noreply.github.com>
2023-12-20 23:00:05 +0800

    Add files via upload

---
+++ day72.py
@@ -0,0 +1,10 @@
+def batas(batasAngka):
+	if batasAngka %2 == 0:
+		print(batasAngka)
+	else:
+		for i in range(1,batasAngka +1):
+			if i %2 1=0:
+				print(i)
+batasAngka = int(input("masukkan batas angka:"))
+batas(batasAngka)
+
