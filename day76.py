8929fbd21539a83306210a2396e4f433d069e7e6
alfian <99625368+Alfiannsptra@users.noreply.github.com>
2023-12-24 20:22:28 +0800

    Add files via upload

---
+++ day76.py
@@ -0,0 +1,10 @@
+aku_badut = False
+dia_spesial = True
+oranglain_badut = True
+
+if aku_badut and dia_spesial:
+    print("wkwkwkwkwk, kamu lucu yal")
+elif oranglain_badut and dia_spesial:
+    print("kamu terlalu seriusi")
+else:
+    print("temenan aja deh")
