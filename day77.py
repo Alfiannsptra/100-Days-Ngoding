3e07cfcace774abca8c538c6e0b3d5e053cf3465
alfian <99625368+Alfiannsptra@users.noreply.github.com>
2023-12-25 16:35:13 +0800

    Add files via upload

--- day77.py
+++ day77.py
@@ -1,8 +1,9 @@
 batas = int(input("masukkan batas: "))
+jumlah =0
 for i in range(1,batas):
 	if i% 2== 0:
 		print(i,"ml game stress")
 	elif i%2!= 0:
 		print(i,"pubg ajaa")
-		
-		
+	jumlah+=i
+print("jumlah =",jumlah)
