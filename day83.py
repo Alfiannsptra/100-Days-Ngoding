666b650c2143c2b209808946a4d38e648237d017
alfian <99625368+Alfiannsptra@users.noreply.github.com>
2023-12-30 21:53:36 +0800

    Add files via upload

---
+++ day83.py
@@ -0,0 +1,10 @@
+tinggi_piramida = int(input('Input tinggi piramida: '))
+print()
+
+for i in range(tinggi_piramida):
+    for j in range(tinggi_piramida - i - 1):
+        print(' ', end=' ')
+     
+    for k in range(i + 1):
+        print('* ', end='')
+    print()
