e1ba77b9582fbc0c476718567848c126834a20fe
alfian <99625368+Alfiannsptra@users.noreply.github.com>
2024-01-13 01:13:12 +0800

    Add files via upload

---
+++ day97.py
@@ -0,0 +1,8 @@
+# Penggunakan f String
+nama = "Alfian Saputra"
+umur = 21
+tinggi = 173
+berat_badan = 64
+status_pernikahan = False
+
+print(f"Nama: {nama}, Umur: {umur}, Tinggi: {tinggi}, Berat Badan: {berat_badan}, Status Pernikahan: {status_pernikahan}")
