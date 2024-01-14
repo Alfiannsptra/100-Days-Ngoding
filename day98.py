4322a46a58ed7acbcd623ac3440cf197dd7921d1
alfian <99625368+Alfiannsptra@users.noreply.github.com>
2024-01-14 20:29:32 +0800

    Add files via upload

---
+++ day98.py
@@ -0,0 +1,11 @@
+nama_karyawan = "Alfiann"
+gaji_pokok = 5000
+bonus = 1000
+
+# Menampilkan informasi gaji
+print(f"Nama: {nama_karyawan}")
+print("Gaji Pokok: ${}".format(gaji_pokok))
+print("Bonus: ${}".format(bonus))
+print("-" * 20) 
+total_gaji = gaji_pokok + bonus
+print(f"Total Gaji: ${total_gaji}")
